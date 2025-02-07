bedc225217f973042ef54bfae418e1e7e82b8067
이민이 <mingramer@gmail.com>
2025-02-07 12:56:12 +0900

    AppConfig 생성해서 의존관계 주입해줌

--- src/test/java/hello/core/member/MemberServiceTest.java
+++ src/test/java/hello/core/member/MemberServiceTest.java
@@ -1,11 +1,19 @@
 package hello.core.member;
 
+import hello.core.AppConfig;
 import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 public class MemberServiceTest {
 
-    MemberService memberService = new MemberServiceImpl();
+    MemberService memberService;
+
+    @BeforeEach
+    public void beforeEach() {
+        AppConfig appConfig = new AppConfig();
+        memberService = appConfig.memberService();
+    }
 
     @Test
     void join() {
@@ -15,6 +23,6 @@ public class MemberServiceTest {
         memberService.join(member);
         Member findMember = memberService.findMember(member.getId());
         //then
-        Assertions.assertThat(member).isEqualTo(findMember );
+        Assertions.assertThat(member).isEqualTo(findMember  );
     }
 }
