103fa7cb5f704b8989d3b10034d72dc7b2dadfdf
이민이 <mingramer@gmail.com>
2025-02-09 09:46:51 +0900

    동일 타입이 두개 이상일때 빈 조회

--- src/test/java/hello/core/beenfind/ApplicationContextBasicFindTest.java
+++ src/test/java/hello/core/beenfind/ApplicationContextBasicFindTest.java
@@ -3,13 +3,13 @@ package hello.core.beenfind;
 import hello.core.AppConfig;
 import hello.core.member.MemberService;
 import hello.core.member.MemberServiceImpl;
-import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.NoSuchBeanDefinitionException;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 import static org.assertj.core.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.*;
 
 class ApplicationContextBasicFindTest {
 
@@ -39,8 +39,7 @@ class ApplicationContextBasicFindTest {
     @Test
     @DisplayName("빈 이름으로 조회X")
     void findBeanByNameX() {
-        MemberService memberService = ac.getBean("xxxxx", MemberService.class);
-        org.junit.jupiter.api.Assertions.assertThrows(NoSuchBeanDefinitionException.class,
+        assertThrows(NoSuchBeanDefinitionException.class,
                 () -> ac.getBean("xxxxx", MemberService.class));
     }
 }
