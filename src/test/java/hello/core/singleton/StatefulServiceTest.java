252cffb9a761ee974ed2e685404e5ccb7cc797f8
이민이 <mingramer@gmail.com>
2025-02-10 23:05:54 +0900

    필요없는 import제거

--- src/test/java/hello/core/singleton/StatefulServiceTest.java
+++ src/test/java/hello/core/singleton/StatefulServiceTest.java
@@ -4,9 +4,6 @@ import org.junit.jupiter.api.Test;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 import org.springframework.context.annotation.Bean;
 
-import static org.assertj.core.api.Assertions.assertThat;
-import static org.junit.jupiter.api.Assertions.*;
-
 class StatefulServiceTest {
 
     @Test
