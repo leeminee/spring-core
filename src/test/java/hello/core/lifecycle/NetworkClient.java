bc76a49913f2755a658ef95d7ebb434ffe9c1528
이민이 <mingramer@gmail.com>
2025-02-11 17:15:42 +0900

    @PostConstruct, @PreDesroy 에 대해 학습. 얘를 거의 사용 권장. 다만, 외부라이브러리를 사용한다면 @Bean의 initMethod옵션을 이용하자.

--- src/test/java/hello/core/lifecycle/NetworkClient.java
+++ src/test/java/hello/core/lifecycle/NetworkClient.java
@@ -1,5 +1,8 @@
 package hello.core.lifecycle;
 
+import jakarta.annotation.PostConstruct;
+import jakarta.annotation.PreDestroy;
+
 public class NetworkClient {
 
     private String url;
@@ -28,6 +31,7 @@ public class NetworkClient {
     }
 
     // 의존관계 주입이 다 끝나면 실행되는 메소드
+    @PostConstruct
     public void init() {
         System.out.println("NetworkClient.init");
         connect();
@@ -35,6 +39,7 @@ public class NetworkClient {
     }
 
     // 빈이 끝날때 호출
+    @PreDestroy
     public void close() {
         System.out.println("NetworkClient.close");
         disconnect();
